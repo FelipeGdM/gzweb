c397da5576bf12976163d6855642fe3917cdc608
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2021-03-27 20:28:33 -0300

    🎨  Format Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,46 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
       // All gz*.js files
       build_src: {
-        src  : ['gz3d/src/*.js'],
-        dest : 'gz3d/build/gz3d.src.js'
+        src: ['gz3d/src/*.js'],
+        dest: 'gz3d/build/gz3d.src.js'
       },
       // * All src except for GUI-related
       // * All needed dependencies
       build_gz3d: {
-        src  : ['gz3d/client/js/include/three.js',
-                'gz3d/client/js/include/three.compat.js',
-                'gz3d/client/js/include/*.js',
-                '!gz3d/client/js/include/three.min.js',
-                '!gz3d/client/js/include/stats.min.js',
-                '!gz3d/client/js/include/roslib.min.js',
-                '!gz3d/client/js/include/jquery-1.9.1.js',
-                '!gz3d/client/js/include/jquery.mobile-1.4.0.min.js',
-                '!gz3d/client/js/include/angular.min.js',
-                '!gz3d/client/js/include/',
-                'gz3d/src/gz*.js',
-                '!gz3d/src/gzgui.js',
-                '!gz3d/src/gzlogplay.js',
-                '!gz3d/src/gzradialmenu.js',
+        src: ['gz3d/client/js/include/three.js',
+          'gz3d/client/js/include/three.compat.js',
+          'gz3d/client/js/include/*.js',
+          '!gz3d/client/js/include/three.min.js',
+          '!gz3d/client/js/include/stats.min.js',
+          '!gz3d/client/js/include/roslib.min.js',
+          '!gz3d/client/js/include/jquery-1.9.1.js',
+          '!gz3d/client/js/include/jquery.mobile-1.4.0.min.js',
+          '!gz3d/client/js/include/angular.min.js',
+          '!gz3d/client/js/include/',
+          'gz3d/src/gz*.js',
+          '!gz3d/src/gzgui.js',
+          '!gz3d/src/gzlogplay.js',
+          '!gz3d/src/gzradialmenu.js',
         ],
-        dest : 'gz3d/build/gz3d.js'
+        dest: 'gz3d/build/gz3d.js'
       },
       // * All src including GUI
       // * All needed dependencies
       build_gui: {
-        src  : ['gz3d/client/js/include/three.js',
-                'gz3d/client/js/include/three.compat.js',
-                'gz3d/client/js/include/*.js',
-                '!gz3d/client/js/include/three.min.js',
-                '!gz3d/client/js/include/stats.min.js',
-                '!gz3d/client/js/include/roslib.min.js',
-                '!gz3d/client/js/include/',
-                'gz3d/src/gz*.js',
+        src: ['gz3d/client/js/include/three.js',
+          'gz3d/client/js/include/three.compat.js',
+          'gz3d/client/js/include/*.js',
+          '!gz3d/client/js/include/three.min.js',
+          '!gz3d/client/js/include/stats.min.js',
+          '!gz3d/client/js/include/roslib.min.js',
+          '!gz3d/client/js/include/',
+          'gz3d/src/gz*.js',
         ],
-        dest : 'gz3d/build/gz3d.gui.js'
+        dest: 'gz3d/build/gz3d.gui.js'
       }
     },
     jshint: {
